7a2d63468ea683b16056132c61d10ad099ee09d6
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:33:01 +0200

    Ex-11 was done

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 try:
-      age=23
+    number1 = float(input("Enter number: "))
+    number2 = float(input("Enter number: "))
+    number3 = 2
+    number4 = float(input(
+        "Якщо ви хочете побачити суму введіть 1, якщо ви хочите побачити різницю введіть 2, якщо ви хочетет побачити середньоарифметичне введіть три, якщо ви хочете побачити добуток введіть 4 "))
+    if (number4 == 1):
+        print(number1 + number2)
+    elif (number4 == 2):
+        print(number1 - number2)
+    elif (number4 == 3):
+        print((number1 + number2) / number3)
+    elif (number4 == 3):
+        print(number1 * number2)
+    else:
+        print("Error")
 except Exception as e:
     print (e)
