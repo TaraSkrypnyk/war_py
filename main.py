4d0116c1fbbbec9e2ea693eb13a3c0c2be1b4c55
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:30:13 +0200

    Ex-8 was done

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 try:
-      age=23
+    number1 = float(input("Enter number: "))
+    if (number1 < 0):
+        print("Error")
+    else:
+        if (number1 % 7 == 0):
+            print(number1, "Number is a multiple of 7")
+        else:
+            print(number1, "Number is not a multiple of 7")
 except Exception as e:
     print (e)
