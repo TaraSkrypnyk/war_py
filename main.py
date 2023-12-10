79d8363a00c2a7c1a01b98b96453d040cfef91b5
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:31:05 +0200

    Ex-9 was done

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 try:
-      age=23
+    number1 = float(input("Enter number: "))
+    number2 = float(input("Enter number: "))
+    if (number1 <= number2):
+        print(number2)
+    else:
+        print(number1)
 except Exception as e:
     print (e)
