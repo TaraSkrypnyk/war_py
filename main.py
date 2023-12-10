b511b8b36328ac525d08a0116f73544b88184863
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:27:19 +0200

    Ex-6 was done

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 try:
-      age=23
+    number1 = float(input("Введіть овжину однієї сторони прямокутника: "))
+    number2 = float(input("Введіть довжину другої сторони прямокутника : "))
+    if (number1 <= 0 or number2 <= 0):
+        print("Error")
+    else:
+        print("Площа дорівнює", number1 * number2)
 except Exception as e:
     print (e)
