de50b846fda7651aa9ac8b84448a7cfc95fc9a66
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:28:57 +0200

    Ex-7 was done

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 try:
-      age=23
+    number1 = float(input("Enter number: "))
+    if (number1 < 0):
+        print("Error")
+    else:
+        if (number1 % 2 == 0):
+            print(number1, "Even number")
+        else:
+            print(number1, "Odd number")
 except Exception as e:
     print (e)
