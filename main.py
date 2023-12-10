15cc862d3f41acb104f129fdfb9c7dd7c4958a26
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:23:09 +0200

    Ex-4 was done

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 try:
-      age=23
+    number1 = float(input("Enter first number: "))
+    number2 = float(input("Enter second number: "))
+    print("Sum", number1 + number2)
+    print("Difference", number1 - number2)
+    print("Product", number1 * number2)
 except Exception as e:
     print (e)
