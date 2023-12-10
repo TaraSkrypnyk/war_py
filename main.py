45e653a1ba66ffb9d7bc6edac79482b5d36b56bc
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:24:16 +0200

    Ex-4 was done

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ try:
     number3 = float(input("Enter third number: "))
     print("Sum", number1 + number2 + number3)
     print("Product", number1 * number2 * number3)
+
 except Exception as e:
     print (e)
