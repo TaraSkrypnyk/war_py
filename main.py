c59eda62fecb6bff6c8c5e3aa3417a8b07619e5f
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:13:48 +0200

    Ex-2 was done

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 try:
-      age=23
+    number1 = float(input("Введіть свою зп за місяць: "))
+    number2 = float(input("Введіть суму місячного платежу за кредитом : "))
+    number3 = float(input("Введіть заборгованість за комунальні посмлуги: "))
+    if (number1 < 0 or number2 < 0 or number3 < 0):
+        print("Error")
+    else:
+        print("Залишок", number4=number1 - number2 - number3)
+
 except Exception as e:
     print (e)
