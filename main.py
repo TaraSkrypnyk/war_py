66057bb9242c9408c895f68af7e4a86e55f75da3
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:20:53 +0200

    Ex-2 was done

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 try:
-      age=23
+    number1 = float(input("Введіть овжину першої діагоналі: "))
+    number2 = float(input("Введіть довжину другої діагоналі : "))
+    number3 = 2
+    if (number1 <= 0 or number2 <= 0):
+        print("Error")
+    else:
+        print("Площа дорівнює", number1 * number2 / number3)
+
 except Exception as e:
     print (e)
