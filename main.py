9033521eefdfd0087ffc40d1e9bccfec0366f355
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-11 00:26:20 +0200

    Ex-5 was done

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 try:
-      age=23
+    number1 = float(input("Enter number: "))
+    number2 = float(input("Enter percent: "))
+    number3 = 100;
+    if (number2 <= 0):
+        print("Error")
+    else:
+        print("Result", number1 / number3 * number2)
 except Exception as e:
     print (e)
